d478ca64a7281a795fe4862e5e14ada7e4db803c
fabricio teixeira <115494324+fabriciosenai@users.noreply.github.com>
2023-03-14 16:22:06 -0300

    Mapeamento das tabelas na Models

---
+++ Configuracao/Models/GrupoUsuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class GrupoUsuario
+    {
+        public int Id { get; set; }
+
+        public string NameGrupo { get; set; }
+
+        public List<Permissao> Permissoes { get; set; }
+    }
+}
---
+++ Configuracao/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string NomeUsuario { get; set; }
+
+        public String Senha { get; set; }
+
+        public string Email { get; set; }
+
+        public string CPF { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public List<GrupoUsuario> Grupos { get; set; }
+
+        public 
+    }
+}
